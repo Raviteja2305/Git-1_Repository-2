b20d3c3ce35e50d9cf4c6824fd14ef4170f018a8
ravi <ravi@DESKTOP-7QNSL67>
2021-06-05 16:54:35 +0530

    Created new Sample Java Class File

---
+++ GitHub_1/src/Repository_1/Sample_2.java
@@ -0,0 +1,12 @@
+package Repository_1;
+
+public class Sample_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Created Sample 2 Java Class");
+
+	}
+
+}
