5bde20966a5e2b520503f34ffafa826834c40a15
ravi <ravi@DESKTOP-7QNSL67>
2021-06-05 16:47:11 +0530

    First Commit for Sample 1 Class in Repository 1 in GitHub 1

---
+++ GitHub_1/src/Repository_1/Sample_1.java
@@ -0,0 +1,12 @@
+package Repository_1;
+
+public class Sample_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is Repository 1 in GitHub 1");
+
+	}
+
+}
