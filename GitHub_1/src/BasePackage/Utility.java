cd1f84bf8bd72c3b31a780723556085346334035
ravi <ravi@DESKTOP-7QNSL67>
2021-06-05 18:39:20 +0530

    removed

--- GitHub_1/src/BasePackage/Utility.java
+++ GitHub_1/src/BasePackage/Utility.java
@@ -4,15 +4,6 @@ public class Utility {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("Check 1");
-		
-
-		System.out.println("Check 11");
-		
-		System.out.println("Check 22");
-		
-		System.out.println("Check33");
 		
 		System.out.println("Check44");
 		
