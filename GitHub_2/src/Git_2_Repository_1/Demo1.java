4b34e5968f3255f3c81bc436f9bbf3411fd3a05a
ravi <ravi@DESKTOP-7QNSL67>
2021-06-06 19:24:59 +0530

    Added print statements Local

--- GitHub_2/src/Git_2_Repository_1/Demo1.java
+++ GitHub_2/src/Git_2_Repository_1/Demo1.java
@@ -7,7 +7,15 @@ public class Demo1 {
 
 		System.out.println("Hello World");
 		
+		System.out.println("Local 1");
 		
+		System.out.println("Local 2");
+		
+		System.out.println("Local 3");
+		
+		System.out.println("Local 4");
+		
+		System.out.println("Local 5");
 
 	}
 
