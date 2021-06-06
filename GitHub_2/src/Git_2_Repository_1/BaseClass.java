c973a94b47036a51c36e2cd65b7053e39992811d
ravi <ravi@DESKTOP-7QNSL67>
2021-06-06 19:22:33 +0530

    created base class and updated demo1

--- GitHub_2/src/Git_2_Repository_1/BaseClass.java
+++ GitHub_2/src/Git_2_Repository_1/BaseClass.java
@@ -1,13 +1,11 @@
 package Git_2_Repository_1;
 
-public class Demo1 {
+public class BaseClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("Hello World");
 		
-		
+		System.out.println("sample");
 
 	}
 
