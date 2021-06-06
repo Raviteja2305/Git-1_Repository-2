6b8afef4668dd7dd4bf068867aae478e6ea83963
ravi <ravi@DESKTOP-7QNSL67>
2021-06-06 18:00:40 +0530

    sdfaf

--- GitHub_1/src/BasePackage/Utility.java
+++ GitHub_1/src/BasePackage/Utility.java
@@ -4,8 +4,6 @@ public class Utility {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-=======
 
 		System.out.println("Check 1");
 
@@ -14,7 +12,6 @@ public class Utility {
 		System.out.println("Check 22");
 		
 		System.out.println("Check33");
->>>>>>> branch 'master' of https://github.com/Raviteja2305/Git-1_Repository-1.git
 		
 		System.out.println("Check44");
 		
