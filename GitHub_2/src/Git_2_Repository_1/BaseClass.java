c6da11b12b42b3d0283b3dc447c625639ddad444
ravi <ravi@DESKTOP-7QNSL67>
2021-06-06 19:41:42 +0530

    removed all

--- GitHub_2/src/Git_2_Repository_1/BaseClass.java
+++ GitHub_2/src/Git_2_Repository_1/BaseClass.java
@@ -6,18 +6,6 @@ public class BaseClass {
 		// TODO Auto-generated method stub
 		
 		System.out.println("sample");
-		
-		System.out.println("Git 1");
-		
-		System.out.println("Git 2");
-		
-		System.out.println("Git 3");
-		
-		System.out.println("Git 4");
-		
-		System.out.println("Git 5");
-		
-		System.out.println("Git 6");
 
 	}
 
