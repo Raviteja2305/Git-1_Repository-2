620850c24d5c7c32ee0c25a04c0eaed64c72c723
ravi <ravi@DESKTOP-7QNSL67>
2021-06-06 18:56:26 +0530

    Push1

---
+++ GitHub_2/src/Git_2_Repository_1/Demo1.java
@@ -0,0 +1,12 @@
+package Git_2_Repository_1;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+
+	}
+
+}
